e4516f3850ff396eefc988f0c0cd31377b59012d
Maggie-sis <lenafrancesca7@gmail.com>
2025-08-25 14:09:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+function update() {
+  const el = document.getElementsByClassName("color");
+  el[0].innerHTML = "pink";
+  el[0].style.color = "pink";
+  el[1].innerHTML = "thistle";
+  el[1].style.color = "thistle";
+  el[2].innerHTML = "tan";
+  el[2].style.color = "tan";
+  el[3].innerHTML = "turquoise";
+  el[3].style.color = "turqouise";
+}
